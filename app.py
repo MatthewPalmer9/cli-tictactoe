bb3c4f55da3af570b37bd5594e0b7a304ff875b1
MacBookPro <matthewpalmer9@outlook.com>
2021-06-30 09:04:42 -0500

    Established players and assigned them to either X or O. Players can choose X or O, and oparameters are in place to prevent faulty values from being passed in.

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ board_values = [
     ' ', ' ', ' '
 ]
 
+player1 = ''
+player2 = ''
+
 def game_board():
     print('\n')
     print(f"{board_values[0]} | {board_values[1]} | {board_values[2]}")
@@ -13,9 +16,35 @@ def game_board():
     print("---------")
     print(f"{board_values[6]} | {board_values[7]} | {board_values[8]}")
 
+def choose_playing_position():
+    global player1
+    global player2
+    answer = input("Player 1 -- Would you like to be X or O? : ").upper()
+
+    while answer not in ['X', 'O']:
+        print("Sorry, not a valid choice \n")
+        answer = input("Player 1 -- Would you like to be X or O? : ").upper()
+
+    print('\n')
+    if answer == 'X':
+        player1 = 'X'
+        player2 = 'O'
+    else:
+        player1 = 'O'
+        player2 = 'X'
+
+    print(f"Player 1, you are {player1}.")
+    print(f"Player 2, you are {player2}.")
+    print('\n')
+    print("Player 1 goes first...")
+    game_board()
+    print('\n')
+
+
 
 
 def start_game():
     game_board()
+    choose_playing_position()
 
 start_game()
