b18ac9ef51cd930ea07d4add6bbaa512b0b8d1ef
MacBookPro <matthewpalmer9@outlook.com>
2021-06-30 08:45:57 -0500

    Developed the game board.

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# Initial file
+# INITIALIZE GAME BOARD #
+board_values = [
+    ' ', ' ', ' ',
+    ' ', ' ', ' ',
+    ' ', ' ', ' '
+]
+
+def game_board():
+    print('\n')
+    print(f"{board_values[0]} | {board_values[1]} | {board_values[2]}")
+    print("---------")
+    print(f"{board_values[3]} | {board_values[4]} | {board_values[5]}")
+    print("---------")
+    print(f"{board_values[0]} | {board_values[1]} | {board_values[2]}")
+
+
+
+def start_game():
+    game_board()
+
+start_game()
